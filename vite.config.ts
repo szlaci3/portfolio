7703e3a492e16dd342892b01ee27bb69cb343da3
Szalai Laci <szalai_laci@yahoo.com>
2025-12-01 08:01:19 +0100

    Add Landing Page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert(), tailwindcss()],
+  server: {
+    origin: "https://localhost:3000",
+    host: "localhost",
+    port: 3000,
+  },
 })
