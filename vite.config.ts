7c95edd0d0b904ee8ad4213ff85bcecc7f288c9a
Szalai Laci <szalai_laci@yahoo.com>
2025-12-01 16:45:33 +0100

    Change layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mkcert from 'vite-plugin-mkcert'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert(), tailwindcss()],
+  plugins: [react(), mkcert()],
   server: {
     origin: "https://localhost:3000",
     host: "localhost",
